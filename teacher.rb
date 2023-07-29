17239a2a325418d698b4ea93ca08dc185bf8a49d
Nylbert Victor <nyldrift@hotmail.com>
2023-07-29 04:02:33 -0300

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(name = 'Unknown', age = nil, specialization)
+  def initialize(specialization, name = 'Unknown', age = nil)
     super(name, age)
     @specialization = specialization
   end
