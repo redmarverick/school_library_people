96e1397bf4cda6aded67c87e7b598c4a4918b26e
redmarverick <nyldrift@hotmail.com>
2023-07-29 22:35:37 -0300

    Basic UI creation
    - Add new classes
    - Create relation between classes
    - Create UI

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,14 @@
 # classroom.rb
-class Classroom
-  attr_accessor :label, :students
 
-  def initialize(label = nil)
+class Classroom
+  attr_reader :label, :students
+
+  def initialize(label)
     @label = label
     @students = []
   end
 
   def add_student(student)
     @students << student
-    student.classroom = self
   end
 end
--- student.rb
+++ student.rb
@@ -1,21 +1,17 @@
+# student.rb
+
 require_relative 'person'
-require_relative 'classroom'
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(name = 'Unknown', age = nil, parent_permission = true, classroom)
+  def initialize(name = 'Unknown', age = nil, classroom = nil, parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
     classroom&.add_student(self)
-    @rentals = []
   end
 
   def play_hooky
     '¯\\_(ツ)_/¯'
-  end
-
-  def add_rental(rental)
-    @rentals << rental
   end
 end
